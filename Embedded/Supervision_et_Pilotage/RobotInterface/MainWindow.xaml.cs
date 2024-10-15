f79d0b80a4da7daf118d7e7791a08e690dfaf329
Table 2 <touloniut@gmail.com>
2024-10-15 17:54:23 +0200

    Début du C#
    Communication Série et visualisation réalisée

---
+++ Embedded/Supervision_et_Pilotage/RobotInterface/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExtendedSerialPort_NS;
+using System.Windows.Threading;
+using System.IO.Ports;
+namespace RobotInterface
+
+
+
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        ExtendedSerialPort serialPort1;
+        string recievedText;
+        DispatcherTimer timerAffichage;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            serialPort1 = new ExtendedSerialPort("COM19", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
+
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick;
+            timerAffichage.Start();
+
+        }
+
+        private void TimerAffichage_Tick(object sender, EventArgs e)
+        {
+                TextBoxReception.Text += recievedText;
+            recievedText = "";
+            
+        }
+
+        private void boutonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void TextBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+
+        public void SendMessage()
+        {
+            serialPort1.WriteLine(TextBoxEmission.Text);
+            TextBoxEmission.Text = "";
+        }
+
+        private void boutonClear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxReception.Text = "";
+        }
+
+
+        public void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            //TextBoxReception.Text = Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            recievedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+        }
+
+
+    }
+}
